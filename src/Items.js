2f8ce24e180c6f8f9b201ec851ff7cbe59b7aedc
Pratik Shahi <shahipr0@gmail.com>
2021-04-04 22:35:29 +0530

    added nav and item component

---
+++ src/Items.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Items() {
+  return (
+    <div>
+      <h1>Item component</h1>
+      <button>Add Item</button>
+      <button>Remove Item</button>
+    </div>
+  );
+}
+export default Items;
---
+++ src/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+function Navbar() {
+  return (
+    <div>
+      <h1>Navbar component :number of item in cart</h1>
+    </div>
+  );
+}
+export default Navbar;
